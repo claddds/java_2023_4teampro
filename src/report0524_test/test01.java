551655e90fee6a4a62b1d2b4043bb57e3989d7df
ady <ady9011@naver.com>
2023-05-24 14:25:10 +0900

    tteesstt

--- src/report0524_test/test01.java
+++ src/report0524_test/test01.java
@@ -4,5 +4,6 @@ public class test01 {
 	public static void main(String[] args) {
 		System.out.println("------test-----");
 		System.out.println("제가 한번 올려보았습니다 ㅋ큐큐");
+		System.out.println("tteesstt");
 	}
 }
