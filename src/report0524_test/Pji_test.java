68fb1d08eae95c1460c4b6db4134e68986cb2b3b
JongInPark <k5whddlf12@gmail.com>
2023-05-26 17:46:30 +0900

    day

--- src/report0524_test/Pji_test.java
+++ src/report0524_test/Pji_test.java
@@ -2,6 +2,7 @@ package report0524_test;
 
 public class Pji_test {
 public static void main(String[] args) {
-	System.out.println("테스트용 확인");
+	System.out.println("테스트용 확인");\
+	System.out.println("머지?? 왜 안됨???");
 }
 }
