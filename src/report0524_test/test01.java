def8b83a3db8aa038792d4a47d594cf941a9a0b9
ady <ady9011@naver.com>
2023-05-26 15:58:28 +0900

    test_X

--- src/report0524_test/test01.java
+++ src/report0524_test/test01.java
@@ -3,7 +3,6 @@ package report0524_test;
 public class test01 {
 	public static void main(String[] args) {
 		System.out.println("------test-----");
-		System.out.println("제가 한번 올려보았습니다 ㅋ큐큐");
-		System.out.println("tteesstt");
+		System.out.println("@@@@@@@@@@");
 	}
 }
