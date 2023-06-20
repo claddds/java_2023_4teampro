c5cf163b60c79c0293ee6736d454ea2510024910
rhj <hyeji5207@gmail.com>
2023-06-21 06:54:00 +0900

    로그인 전 main화면-2

--- src/view/Main_logbefore.java
+++ src/view/Main_logbefore.java
@@ -15,6 +15,9 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import net.miginfocom.swing.MigLayout;
 
 public class Main_logbefore {
 
@@ -50,9 +53,10 @@ public class Main_logbefore {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 800, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[10px][152px][624px,grow]", "[43px][57.00px][][][][10px][][][][][][][][][][][][][][][][grow]"));
 		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		JPanel topbutton_panel = new JPanel();
+		frame.getContentPane().add(topbutton_panel, "cell 0 0 3 1,alignx left,aligny top");
 		
 		JButton mticket_button = new JButton("모바일티켓");
 		mticket_button.setFont(new Font("돋움", Font.BOLD, 20));
@@ -66,10 +70,10 @@ public class Main_logbefore {
 		
 		JButton join_button = new JButton("회원가입");
 		join_button.setFont(new Font("돋움", Font.BOLD, 20));
-		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
+		GroupLayout gl_topbutton_panel = new GroupLayout(topbutton_panel);
+		gl_topbutton_panel.setHorizontalGroup(
+			gl_topbutton_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_topbutton_panel.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(mticket_button, GroupLayout.PREFERRED_SIZE, 157, GroupLayout.PREFERRED_SIZE)
 					.addGap(330)
@@ -78,18 +82,43 @@ public class Main_logbefore {
 					.addComponent(join_button, GroupLayout.PREFERRED_SIZE, 127, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+		gl_topbutton_panel.setVerticalGroup(
+			gl_topbutton_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_topbutton_panel.createSequentialGroup()
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 					.addComponent(mticket_button))
-				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+				.addGroup(Alignment.TRAILING, gl_topbutton_panel.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+					.addGroup(gl_topbutton_panel.createParallelGroup(Alignment.BASELINE)
 						.addComponent(join_button)
 						.addComponent(login_button)))
 		);
-		panel.setLayout(gl_panel);
+		topbutton_panel.setLayout(gl_topbutton_panel);
+		
+		JPanel logo_panel = new JPanel();
+		frame.getContentPane().add(logo_panel, "cell 1 1 2 1,alignx right,aligny top");
+		GridBagLayout gbl_logo_panel = new GridBagLayout();
+		gbl_logo_panel.columnWidths = new int[]{437, 0};
+		gbl_logo_panel.rowHeights = new int[]{79, 0};
+		gbl_logo_panel.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_logo_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		logo_panel.setLayout(gbl_logo_panel);
+		
+		JButton logo_button = new JButton("4딸라- 시네마");
+		frame.getContentPane().add(logo_button, "cell 2 2");
+		logo_button.setFont(new Font("돋움", Font.BOLD, 60));
+		
+		JPanel book_snack_panel = new JPanel();
+		frame.getContentPane().add(book_snack_panel, "cell 1 21 2 1,grow");
+		book_snack_panel.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JButton book_button = new JButton("예매");
+		book_button.setFont(new Font("돋움", Font.BOLD, 20));
+		book_snack_panel.add(book_button);
+		
+		JButton snackbar_button = new JButton("매점");
+		snackbar_button.setFont(new Font("돋움", Font.BOLD, 20));
+		book_snack_panel.add(snackbar_button);
 	}
 
 }
