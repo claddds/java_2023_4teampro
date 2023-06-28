106fb2d9ea575d0afc58c127be64c6bd0e202ee6
ryuhyeji <hyeji5207@gmail.com>
2023-06-28 19:24:44 +0900

    0628 학원에서

--- src/view/Sign_in.java
+++ src/view/Sign_in.java
@@ -1,8 +1,17 @@
 package view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Sign_in extends JFrame{
+	private JTextField signin_id_tf;
+	private JTextField signin_pw_tf;
 	public Sign_in() {
 		super("영화예매프로그램");
 		
@@ -10,9 +19,60 @@ public class Sign_in extends JFrame{
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JPanel signin_login_panel = new JPanel();
+		signin_login_panel.setBounds(0, 0, 784, 112);
+		getContentPane().add(signin_login_panel);
+		signin_login_panel.setLayout(null);
+		
+		JLabel signin_login_label = new JLabel("로그인");
+		signin_login_label.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+		signin_login_label.setHorizontalAlignment(SwingConstants.CENTER);
+		signin_login_label.setBounds(298, 38, 165, 44);
+		signin_login_panel.add(signin_login_label);
+		
+		JPanel signin_id_pw_panel = new JPanel();
+		signin_id_pw_panel.setBounds(0, 111, 784, 112);
+		getContentPane().add(signin_id_pw_panel);
+		signin_id_pw_panel.setLayout(null);
+		
+		JLabel signin_id_label = new JLabel("아이디");
+		signin_id_label.setHorizontalAlignment(SwingConstants.CENTER);
+		signin_id_label.setFont(new Font("맑은 고딕", Font.BOLD, 16));
+		signin_id_label.setBounds(200, 21, 90, 30);
+		signin_id_pw_panel.add(signin_id_label);
+		
+		JLabel signin_pw_label = new JLabel("비밀번호");
+		signin_pw_label.setFont(new Font("맑은 고딕", Font.BOLD, 16));
+		signin_pw_label.setHorizontalAlignment(SwingConstants.CENTER);
+		signin_pw_label.setBounds(200, 60, 90, 30);
+		signin_id_pw_panel.add(signin_pw_label);
+		
+		signin_id_tf = new JTextField();
+		signin_id_tf.setBounds(283, 27, 220, 21);
+		signin_id_pw_panel.add(signin_id_tf);
+		signin_id_tf.setColumns(10);
+		
+		signin_pw_tf = new JTextField();
+		signin_pw_tf.setBounds(286, 65, 220, 21);
+		signin_id_pw_panel.add(signin_pw_tf);
+		signin_pw_tf.setColumns(10);
+		
+		JPanel signin_login_bt_panel = new JPanel();
+		signin_login_bt_panel.setBounds(0, 221, 784, 82);
+		getContentPane().add(signin_login_bt_panel);
+		signin_login_bt_panel.setLayout(null);
+		
+		JButton signin_login_bt = new JButton("로그인");
+		signin_login_bt.setBounds(252, 22, 97, 23);
+		signin_login_bt_panel.add(signin_login_bt);
+		
+		JButton signin_cancel_bt = new JButton("취소");
+		signin_cancel_bt.setBounds(406, 23, 97, 23);
+		signin_login_bt_panel.add(signin_cancel_bt);
 	}
 	public static void main(String[] args) {
 		new Sign_in();
 	}
-
 }
--- src/view/Sign_up.java
+++ src/view/Sign_up.java
@@ -1,8 +1,20 @@
 package view;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Sign_up extends JFrame{
+	private JTextField signup_id_tf;
+	private JTextField signup_pw_tf;
+	private JTextField signup_name_tf;
+	private JTextField signup_birth_tf;
+	private JTextField signup_phone_tf;
 	public Sign_up() {
 		super("영화예매프로그램");
 		
@@ -10,8 +22,96 @@ public class Sign_up extends JFrame{
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JPanel signup_signup_panel = new JPanel();
+		signup_signup_panel.setBounds(0, 0, 784, 112);
+		getContentPane().add(signup_signup_panel);
+		signup_signup_panel.setLayout(null);
+		
+		JLabel signup_label = new JLabel("회원가입");
+		signup_label.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+		signup_label.setHorizontalAlignment(SwingConstants.CENTER);
+		signup_label.setBounds(298, 38, 165, 44);
+		signup_signup_panel.add(signup_label);
+		
+		JPanel signup_signup_w_panel = new JPanel();
+		signup_signup_w_panel.setBounds(0, 111, 784, 253);
+		getContentPane().add(signup_signup_w_panel);
+		signup_signup_w_panel.setLayout(null);
+		
+		JLabel signup_id_label = new JLabel("아이디");
+		signup_id_label.setHorizontalAlignment(SwingConstants.CENTER);
+		signup_id_label.setFont(new Font("맑은 고딕", Font.BOLD, 16));
+		signup_id_label.setBounds(200, 21, 90, 30);
+		signup_signup_w_panel.add(signup_id_label);
+		
+		JLabel signup_pw_label = new JLabel("비밀번호");
+		signup_pw_label.setFont(new Font("맑은 고딕", Font.BOLD, 16));
+		signup_pw_label.setHorizontalAlignment(SwingConstants.CENTER);
+		signup_pw_label.setBounds(200, 60, 90, 30);
+		signup_signup_w_panel.add(signup_pw_label);
+		
+		signup_id_tf = new JTextField();
+		signup_id_tf.setBounds(286, 29, 220, 21);
+		signup_signup_w_panel.add(signup_id_tf);
+		signup_id_tf.setColumns(10);
+		
+		signup_pw_tf = new JTextField();
+		signup_pw_tf.setBounds(289, 68, 220, 21);
+		signup_signup_w_panel.add(signup_pw_tf);
+		signup_pw_tf.setColumns(10);
+		
+		JLabel signup_name_label = new JLabel("이름");
+		signup_name_label.setHorizontalAlignment(SwingConstants.CENTER);
+		signup_name_label.setFont(new Font("맑은 고딕", Font.BOLD, 16));
+		signup_name_label.setBounds(202, 100, 90, 30);
+		signup_signup_w_panel.add(signup_name_label);
+		
+		JLabel signup_birth_label = new JLabel("생년월일");
+		signup_birth_label.setHorizontalAlignment(SwingConstants.CENTER);
+		signup_birth_label.setFont(new Font("맑은 고딕", Font.BOLD, 16));
+		signup_birth_label.setBounds(205, 143, 90, 30);
+		signup_signup_w_panel.add(signup_birth_label);
+		
+		JLabel signup_phone_label = new JLabel("핸드폰 번호");
+		signup_phone_label.setHorizontalAlignment(SwingConstants.CENTER);
+		signup_phone_label.setFont(new Font("맑은 고딕", Font.BOLD, 16));
+		signup_phone_label.setBounds(189, 183, 90, 30);
+		signup_signup_w_panel.add(signup_phone_label);
+		
+		signup_name_tf = new JTextField();
+		signup_name_tf.setBounds(289, 105, 220, 21);
+		signup_signup_w_panel.add(signup_name_tf);
+		signup_name_tf.setColumns(10);
+		
+		signup_birth_tf = new JTextField();
+		signup_birth_tf.setColumns(10);
+		signup_birth_tf.setBounds(289, 151, 220, 21);
+		signup_signup_w_panel.add(signup_birth_tf);
+		
+		signup_phone_tf = new JTextField();
+		signup_phone_tf.setColumns(10);
+		signup_phone_tf.setBounds(289, 191, 220, 21);
+		signup_signup_w_panel.add(signup_phone_tf);
+		
+		JButton signup_idcheck_bt = new JButton("중복확인");
+		signup_idcheck_bt.setBounds(552, 28, 97, 23);
+		signup_signup_w_panel.add(signup_idcheck_bt);
+		
+		JPanel signup_bt_panel = new JPanel();
+		signup_bt_panel.setBounds(0, 364, 784, 82);
+		getContentPane().add(signup_bt_panel);
+		signup_bt_panel.setLayout(null);
+		
+		JButton signup_signup_bt = new JButton("회원가입");
+		signup_signup_bt.setBounds(252, 22, 97, 23);
+		signup_bt_panel.add(signup_signup_bt);
+		
+		JButton signup_cancel_bt = new JButton("취소");
+		signup_cancel_bt.setBounds(406, 23, 97, 23);
+		signup_bt_panel.add(signup_cancel_bt);
 	}
-	
 	public static void main(String[] args) {
 		new Sign_up();
 	}
